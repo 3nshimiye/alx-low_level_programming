0f7b946e2a59bc27eaa906f4dcd3c0f058b1b5d4
Emmanuel Nshimiyimana <3nshimiye@gmail.com>
2022-05-10 02:32:13 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+
 /**
  * malloc_checked - memory allocated
  * @b: memory size
  * Return: SUCCESS for pointer to the allocated memory
  * 98 if in case of insufficient memory
  */
-
-#include <stdlib.h>
-#include "main.h"
 
 void *malloc_checked(unsigned int b)
 {
